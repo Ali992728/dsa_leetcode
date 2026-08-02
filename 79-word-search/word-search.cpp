431f597552c1db9464b692162b511cb1fb04b6f5
Ali992728 <154454404+Ali992728@users.noreply.github.com>
2026-08-02 17:48:02 +0600

    Time: 838 ms (17.74%) | Memory: 11.5 MB (42.96%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board,int i,int j,string word,int count) {
+        if(count==word.size()) return true;
+
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || word[count]!=board[i][j]) return false;
+
+        char temp=board[i][j];
+        board[i][j]='*';
+
+        bool isfound=dfs(board,i+1,j,word,count+1) || dfs(board,i-1,j,word,count+1) || dfs(board,i,j+1,word,count+1) || dfs(board,i,j-1,word,count+1);
+
+        board[i][j]=temp;
+        return isfound;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++) {
+            for(int j=0;j<board[0].size();j++) {
+                if(word[0]==board[i][j] && dfs(board,i,j,word,0)) return true;
+            }
+        }
+        return false;
+    }
+};
